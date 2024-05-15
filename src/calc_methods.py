15f72e9a4bf05ff5746a0eb109e8085e58949b77
sadness52r <bukin.k2017@gmail.com>
2024-05-15 19:58:43 +0300

    first commit

---
+++ src/calc_methods.py
@@ -0,0 +1,14 @@
+def summ(a: int, b: int) -> int:
+    return a + b
+def sub(a: int, b: int) -> int:
+    return a - b
+def mult(a: int, b: int) -> int:
+    return a * b
+def div(a: int, b: int) -> float:
+    if b == 0:
+        return 999999999999999999999
+    return a / b
+def mod(a: int, b: int) -> int:
+    if b == 0:
+        return 999999999999999999999
+    return a % b
---
+++ test_calc.py
@@ -0,0 +1,64 @@
+import pytest
+from src.calc_methods import *
+
+@pytest.mark.parametrize(
+    ('a', 'b', 'res'), [
+        (2, 3, 5),
+        (0, 0, 0),
+        (-4, 6, 2),
+        (-8, -8, -16),
+        (-25, 25, 0),
+    ]
+)
+def test_summ(a, b, res):
+    assert summ(a, b) == res
+
+@pytest.mark.parametrize(
+    ('a', 'b', 'res'), [
+        (2, 3, -1),
+        (0, 0, 0),
+        (-4, 6, -10),
+        (-8, -8, 0),
+        (-25, 25, -50),
+    ]
+)
+def test_sub(a, b, res):
+    assert sub(a, b) == res
+
+@pytest.mark.parametrize(
+    ('a', 'b', 'res'), [
+        (2, 3, 6),
+        (0, 0, 0),
+        (-4, 6, -24),
+        (-8, -8, 64),
+        (-25, 25, -625),
+    ]
+)
+def test_mult(a, b, res):
+    assert mult(a, b) == res
+
+@pytest.mark.parametrize(
+    ('a', 'b', 'res'), [
+        (2, 3, 2/3),
+        (0, 0, 999999999999999999999),
+        (0, 5, 0),
+        (-4, 6, -4 / 6),
+        (-8, -8, 1),
+        (-25, 25, -1),
+    ]
+)
+def test_div(a, b, res):
+    assert div(a, b) == res
+
+@pytest.mark.parametrize(
+    ('a', 'b', 'res'), [
+        (2, 3, 2),
+        (0, 0, 999999999999999999999),
+        (0, 5, 0),
+        (-4, 6, -4 % 6),
+        (-8, -8, -8 % -8),
+        (-25, 25, -25 % 25),
+    ]
+)
+def test_mod(a, b, res):
+    assert mod(a, b) == res
